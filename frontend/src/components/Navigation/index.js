4c59b9036a1ca4e5b499f5be2b65a678a6035d8b
Stiven Kang <kdaegyung@gmail.com>
2023-02-12 17:14:35 -0500

    started css for home page

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import logo from '../../assets/spotify-logo-inverted.png';
 import './Navigation.css';
 
 function Navigation() {
@@ -18,19 +19,41 @@ function Navigation() {
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <div className='navTop'>
+          <NavLink className='navSignup' to="/signup">Sign Up</NavLink>
+          <NavLink className='navLogin' to="/login">Log In</NavLink>
+        </div>
       </>
     );
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
-        {sessionLinks}
-      </li>
-    </ul>
+    // <div className='home'>
+    //   <div className='sidebar'>
+    //     <p>sidebar</p>
+    //   </div>
+    //   <ul>
+    //     <li className='topButtons'>
+    //       <NavLink exact to="/">Home</NavLink>
+    //       {sessionLinks}
+    //     </li>
+    //   </ul>
+    // </div>
+
+    <div className='home'>
+      <div className='sidebar'>
+        <img className='homeLogo' src={logo} alt='' />
+        <br/>
+        <NavLink className='homeButton' exact to="/"><i class="fa-solid fa-house"></i><span style={{marginLeft: '15px'}}>Home</span></NavLink>
+        <br/>
+        <NavLink className='searchButton' exact to="/search"><i class="fa-solid fa-magnifying-glass"></i><span style={{marginLeft: '15px'}}>Search</span></NavLink>
+      </div>
+      <ul>
+        <li className='topButtons'>
+          {sessionLinks}
+        </li>
+      </ul>
+    </div>
   );
 }
 
