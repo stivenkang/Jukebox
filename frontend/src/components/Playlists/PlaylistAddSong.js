29fd77e16937c984ae46b35bed0f07e09b923db2
Stiven Kang <kdaegyung@gmail.com>
2023-04-24 23:06:25 -0400

    Creating playlist song visible component

---
+++ frontend/src/components/Playlists/PlaylistAddSong.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import './Playlist.css';
+
+function PlaylistAddSong() {
+    const history = useHistory();
+
+    const artists = useSelector(state => state.artists ? Object.values(state.artists) : []);
+    const albums = useSelector(state => state.albums ? Object.values(state.albums) : []);
+    const songs = useSelector(state => state.songs ? Object.values(state.songs) : []);
+
+    useEffect(() => {
+        dispatch(fetchArtists())
+        dispatch(fetchAlbums())
+        dispatch(fetchSongs())
+    }, []);
+
+    // useEffect(() => {
+
+    // })
+
+    const artistName = artists.find(artist => artist.id === albums.artistId)?.name;
+
+    const [input, setInput] = useState('');
+
+
+    return (
+        <div className='plAdd'>
+            <img className='plAddImg' src={} alt='' />
+            <div>
+
+                {/* Name of artist, album, song, etc */}
+                {/* Actual type (Artist, Album, Actual Artist Name) */}
+            </div>
+        </div>
+    )
+}
+
+export default PlaylistAddSong;
